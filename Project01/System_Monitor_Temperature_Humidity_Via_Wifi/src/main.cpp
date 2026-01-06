240595804601456e838756aa3e608a478fba2db4
Thanhtan2108 <Nthanhtan2004@gmail.com>
2026-01-06 10:09:52 +0700

    test function for module Connect_Wifi to check wifi was connected?

--- Project01/System_Monitor_Temperature_Humidity_Via_Wifi/src/main.cpp
+++ Project01/System_Monitor_Temperature_Humidity_Via_Wifi/src/main.cpp
@@ -1,8 +1,35 @@
 #include <Arduino.h>
+#include <Common_Config.h>
+#include <Connect_Wifi.h>
+
+
+void manager_status_connect_wifi();
 
 void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  pinMode(LED_STATUS_WIFI, OUTPUT);
+  digitalWrite(LED_STATUS_WIFI, LOW);
+
+  Serial.println("\n=== ESP32 Temperature & Humidity Monitor ===");
+    
+  // Initialize WiFi
+  wifi_init();
+
+  Serial.println("System initialization complete!");
 }
 
 void loop() {
+  manager_status_connect_wifi();
+}
 
+void manager_status_connect_wifi() {
+  if (wifi_is_connected()) {
+    digitalWrite(LED_STATUS_WIFI, HIGH);
+  } else {
+    digitalWrite(LED_STATUS_WIFI, LOW);
+    wifi_reconnect();
+  }
+  delay(2000);
 }
