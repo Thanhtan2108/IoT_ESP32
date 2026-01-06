66631ff1cb2a368013a7f708b1b8e6ce9fc41907
Thanhtan2108 <Nthanhtan2004@gmail.com>
2026-01-06 09:37:55 +0700

    develop for function connect_wifi of module Common_Config

---
+++ Project01/System_Monitor_Temperature_Humidity_Via_Wifi/lib/Common_Config/Common_Config.cpp
@@ -0,0 +1,4 @@
+#include "Common_Config.h"
+
+const char* SSID_WIFI = "Thanhtan";
+const char* PASSWORD_WIFI = "12345678";
---
+++ Project01/System_Monitor_Temperature_Humidity_Via_Wifi/lib/Common_Config/Common_Config.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_CONFIG_H
+#define COMMON_CONFIG_H
+
+extern const char* SSID_WIFI;
+extern const char* PASSWORD_WIFI;
+
+/*
+Thay vì dùng extern + .cpp, dùng từ khóa constexpr để không cần tạo .cpp
+constexpr const char* SSID_WIFI = "Thanhtan";
+constexpr const char* PASSWORD_WIFI = "12345678";
+*/
+
+#define LED_STATUS_WIFI 2
+
+#endif
